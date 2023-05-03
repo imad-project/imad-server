4a4fc488a515c86f35bca9a93569b6caebfc341d
SeungWoo Ryu <swstar21c@gmail.com>
2023-05-03 16:00:26 +0900

    #1 - spring boot 프로젝트 생성
    - spring.start.io 사용하여 프로젝트를 생성함

---
+++ src/test/java/com/ncookie/imad/ImadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ncookie.imad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImadServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
