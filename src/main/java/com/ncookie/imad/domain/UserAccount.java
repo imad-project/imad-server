aeb620977c551a43e7243da2d175cab1299e464d
SeungWoo Ryu <swstar21c@gmail.com>
2023-05-06 17:27:10 +0900

    #6 - 사용자 계정 도메인을 entity 구현
    - UserAccount entity을 통해 사용자 계정 도메인을 구현했음
    - 엔티티 구성에 필요한 성별, 역할(게스트, 일반사용자), auth 제공자 등을 enum type으로 만듦
    - 데이터 생성시간과 수정시간 메타 데이터를 가지고 있는 클래스 구현

---
+++ src/main/java/com/ncookie/imad/domain/UserAccount.java
@@ -0,0 +1,71 @@
+package com.ncookie.imad.domain;
+
+import com.ncookie.imad.domain.type.AuthProvider;
+import com.ncookie.imad.domain.type.Gender;
+import com.ncookie.imad.domain.type.Role;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@ToString
+@Table(indexes = {
+        @Index(columnList = "userId"),
+        @Index(columnList = "email", unique = true),
+})
+@Entity
+public class UserAccount extends BaseTimeEntity {
+    @Id
+    private String userId;
+
+    @Setter @Column(nullable = false, length = 50) private String nickname;
+    @Setter @Column(length = 50) private String userPassword;
+
+    @Setter private String email;               // 이메일
+    @Setter private Gender gender;              // 성별, 0 : 남자, 1 : 여자
+    @Setter private int ageRange;               // 연령대
+    @Setter private String profileImageUrl;     // 프로필 이미지(URL)
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private AuthProvider authProvider;
+
+    protected UserAccount() {}
+
+    private UserAccount(String userId,
+                        String nickname,
+                        String userPassword,
+                        Gender gender,
+                        String email,
+                        int ageRange,
+                        String profileImage,
+                        Role role,
+                        AuthProvider authProvider) {
+        this.userId = userId;
+        this.nickname = nickname;
+        this.userPassword = userPassword;
+        this.gender = gender;
+        this.email = email;
+        this.ageRange = ageRange;
+        this.profileImageUrl = profileImage;
+        this.role = role;
+        this.authProvider = authProvider;
+    }
+
+    public static UserAccount of(String userId,
+                                 String nickname,
+                                 String userPassword,
+                                 Gender gender,
+                                 String email,
+                                 int ageRange,
+                                 String profileImage,
+                                 Role role,
+                                 AuthProvider authProvider) {
+        return new UserAccount(userId, nickname, userPassword, gender, email, ageRange, profileImage, role, authProvider);
+    }
+}
---
+++ src/main/java/com/ncookie/imad/domain/type/Gender.java
@@ -0,0 +1,14 @@
+package com.ncookie.imad.domain.type;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Gender {
+    MALE("MALE", "남자"),
+    FEMALE("FEMALE", "여자");
+    
+    private final String  key;
+    private final String gender;
+}
