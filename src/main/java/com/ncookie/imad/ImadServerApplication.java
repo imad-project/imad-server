d5eb9c4b931831ee33e6fb94219088980ca086a9
NCookies <swstar21c@gmail.com>
2023-06-03 14:51:02 +0900

    AWS EC2 배포 자동화 작업
    - github master 브랜치에 push가 되면 github actions를 통해 자동으로 AWS EC2에 배포가 되도록 설정 파일들을 추가함
    - 기존의 spring-practice 프로젝트를 참고하여 스크립트 파일 및 설정들을 추가함

--- src/main/java/com/ncookie/imad/ImadServerApplication.java
+++ src/main/java/com/ncookie/imad/ImadServerApplication.java
@@ -2,7 +2,6 @@ package com.ncookie.imad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -12,5 +11,4 @@ public class ImadServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ImadServerApplication.class, args);
 	}
-
 }
