da0c82444598c2efc54b9d73b8dab4d8ffac8ca0
NCookies <swstar21c@gmail.com>
2023-06-29 18:34:41 +0900

    #13 - 애플 로그인 controller 추가
    - 애플로부터 유저 로그인 시 return url을 받기 위한 컨트롤러 생성

---
+++ src/main/java/com/ncookie/imad/domain/user/controller/AppleController.java
@@ -0,0 +1,24 @@
+package com.ncookie.imad.domain.user.controller;
+
+import com.ncookie.imad.global.oauth2.dto.AppleDTO;
+import com.ncookie.imad.global.oauth2.dto.MsgEntity;
+import com.ncookie.imad.global.oauth2.service.AppleService;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class AppleController {
+    private final AppleService appleService;
+
+    @PostMapping("/callback/apple")
+    public ResponseEntity<MsgEntity> callback(HttpServletRequest request) throws Exception {
+        AppleDTO appleInfo = appleService.getAppleInfo(request.getParameter("code"));
+
+        return ResponseEntity.ok()
+                .body(new MsgEntity("Success", appleInfo));
+    }
+}
