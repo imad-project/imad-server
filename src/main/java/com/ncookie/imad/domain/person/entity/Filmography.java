f0d71c98c75230bfc08c73875d102bba7fa7c6c2
NCookies <swstar21c@gmail.com>
2023-07-26 23:42:33 +0900

    #21 - Person, Filmography entity 구현
    - 필드 구현 및 주석 추가

--- src/main/java/com/ncookie/imad/domain/person/entity/Filmography.java
+++ src/main/java/com/ncookie/imad/domain/person/entity/Filmography.java
@@ -1,7 +1,13 @@
 package com.ncookie.imad.domain.person.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Filmography {
     @Id
@@ -11,4 +17,24 @@ public class Filmography {
     @ManyToOne
     @JoinColumn(name = "person_id")
     private Person person;
+
+    @Setter
+    // 담당부서
+    // 감독이나 작가, 기타 스태프 등의 인물만 가지고 있는 속성임
+    // Ex) Production, Wriging, Camera, etc.
+    private String department;
+
+    @Setter
+    // 특정 부서 또는 분야로 잘 알려져 있는 정보
+    // Ex) Acting, Writing, Camera, Crew, etc.
+    private String knownForDepartment;
+
+    @Setter
+    // 맡은 업무
+    // Ex) Co-Executive Producer, Production Coordinaotr, Camera Operator, etc.
+    private String job;
+
+    @Setter
+    // 배역명. 배우들만 해당됨
+    private String characterName;
 }
--- src/main/java/com/ncookie/imad/domain/person/entity/Person.java
+++ src/main/java/com/ncookie/imad/domain/person/entity/Person.java
@@ -1,11 +1,17 @@
 package com.ncookie.imad.domain.person.entity;
 
+import com.ncookie.imad.domain.user.entity.Gender;
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Getter
+@Builder
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Person {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -13,4 +19,10 @@ public class Person {
 
     @OneToMany(mappedBy = "person")
     private List<Filmography> filmography = new ArrayList<>();
+
+    private String original_name;
+    @Setter private String translated_name;
+
+    private Gender gender;
+    private String profile_path;
 }
