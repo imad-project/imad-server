761e899f754b082d3b52967b906b235ed576a499
NCookies <swstar21c@gmail.com>
2023-07-28 21:10:10 +0900

    #21 - JpaRepository 기본키인 Long으로 변경

--- src/main/java/com/ncookie/imad/domain/user/repository/UserAccountRepository.java
+++ src/main/java/com/ncookie/imad/domain/user/repository/UserAccountRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
+public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
     Optional<UserAccount> findByEmail(String email);
     Optional<UserAccount> findByNickname(String nickname);
     Optional<UserAccount> findByRefreshToken(String refreshToken);
