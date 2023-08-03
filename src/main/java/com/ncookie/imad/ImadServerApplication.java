b032688dcc18d02a38676382670d440d8d73950d
NCookies <swstar21c@gmail.com>
2023-08-04 03:19:07 +0900

    #21 - OpenFeign 활성화(import 구문 추가)

--- src/main/java/com/ncookie/imad/ImadServerApplication.java
+++ src/main/java/com/ncookie/imad/ImadServerApplication.java
@@ -2,10 +2,11 @@ package com.ncookie.imad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
 @EnableFeignClients
+@EnableJpaAuditing
 @SpringBootApplication
 public class ImadServerApplication {
 
