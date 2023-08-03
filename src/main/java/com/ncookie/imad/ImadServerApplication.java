a4cdb12eda310c360397b303e1bce1ce4fd46155
NCookies <swstar21c@gmail.com>
2023-08-03 23:50:37 +0900

    #21 - OpenFeign 활성화

--- src/main/java/com/ncookie/imad/ImadServerApplication.java
+++ src/main/java/com/ncookie/imad/ImadServerApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@EnableFeignClients
 @SpringBootApplication
 public class ImadServerApplication {
 
