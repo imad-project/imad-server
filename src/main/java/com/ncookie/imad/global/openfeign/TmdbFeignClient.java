8835cb6744851fcdcb7d7fedfd8ac2f151f79af7
NCookies <swstar21c@gmail.com>
2023-08-09 00:54:07 +0900

    #21 - 항상 고정적으로 사용하는 header 값을 @Header 어노테이션으로 설정
    - `Accept: application/json` 헤더는 항상 똑같이 들어가기 때문에 매개변수가 아닌 어노테이션에서 설정해주었음

--- src/main/java/com/ncookie/imad/global/openfeign/TmdbFeignClient.java
+++ src/main/java/com/ncookie/imad/global/openfeign/TmdbFeignClient.java
@@ -1,20 +1,22 @@
 package com.ncookie.imad.global.openfeign;
 
 import com.ncookie.imad.domain.contents.dto.SearchResponse;
+import feign.Headers;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
 
-@FeignClient(name = "tmdb", url = "${tmdb.api.url}")
+@FeignClient(name = "tmdb", url = "${tmdb.api.api-url}")
 public interface TmdbFeignClient {
 
     // 키워드 통합 검색(영화, TV, 시리즈, 인물 등 포함)
-    @GetMapping("/search/multi")
+    @GetMapping(value = "/search/multi")
+    @Headers("Accept: " + MediaType.APPLICATION_JSON_VALUE)
     SearchResponse searchMultiByQuery(
             @RequestHeader(value = "Authorization") String apiKey,
-            @RequestHeader(value = "Accept") String acceptHeader,
             @RequestParam(value = "query") String query,
             @RequestParam(value = "include_adult") boolean includeAdult,
             @RequestParam(value = "language") String language,
