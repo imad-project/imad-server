155e4f5058e566f7d0628704a514ecf26d3e4df8
NCookies <swstar21c@gmail.com>
2023-08-09 00:56:36 +0900

    #21 - TMDB API를 OpenFeign으로 호출하는 관련 메소드들을 모아둔 클래스를 생성하였음
    - 코드가 반복되거나 기타 추가 기능 부분을 별도의 메소드로 구현하기로 함
    - ContentsService 외에 다른 서비스에서도 api를 사용할 예정이기 때문에 하나의 파일에서 메소드를 관리하는게 효율적이라고 판단함

--- src/main/java/com/ncookie/imad/domain/contents/service/ContentsService.java
+++ src/main/java/com/ncookie/imad/domain/contents/service/ContentsService.java
@@ -1,11 +1,8 @@
 package com.ncookie.imad.domain.contents.service;
 
-import com.ncookie.imad.domain.contents.dto.SearchResponse;
-import com.ncookie.imad.global.openfeign.TmdbFeignClient;
+import com.ncookie.imad.global.openfeign.TmdbApiClient;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 
 
@@ -13,20 +10,9 @@ import org.springframework.stereotype.Service;
 @Transactional
 @Service
 public class ContentsService {
-    private final TmdbFeignClient feignClient;
-
-    @Value("${tmdb.api.api-key}")
-    private String apiKey;
-
+    private final TmdbApiClient apiClient;
 
     public void searchKeywords() {
-        SearchResponse response = feignClient.searchMultiByQuery(
-                apiKey,
-                MediaType.APPLICATION_JSON_VALUE,
-                "breaking",
-                true,
-                "ko-kr",
-                1
-        );
+        apiClient.searchMultiByQuery("귀멸");
     }
 }
---
+++ src/main/java/com/ncookie/imad/global/openfeign/TmdbApiClient.java
@@ -0,0 +1,25 @@
+package com.ncookie.imad.global.openfeign;
+
+import com.ncookie.imad.domain.contents.dto.SearchResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+// TMDB API를 사용하기 위한 메소드들을 모아놓은 클래스
+@RequiredArgsConstructor
+@Component
+public class TmdbApiClient {
+    private final TmdbFeignClient feignClient;
+    private final TmdbApiProperties apiProperties;
+
+    private final String language = "ko-kr";
+
+    public SearchResponse searchMultiByQuery(String query) {
+        return feignClient.searchMultiByQuery(
+                apiProperties.getApiKey(),
+                query,
+                true,
+                language,
+                1
+        );
+    }
+}
