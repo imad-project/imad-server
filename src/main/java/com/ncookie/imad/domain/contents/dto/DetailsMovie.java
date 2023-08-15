f68221bdc95efe722b63091e6f8034c4eb4398f3
NCookies <swstar21c@gmail.com>
2023-08-16 01:11:51 +0900

    #21 - DTO 클래스 패키지 경로 수정

--- src/main/java/com/ncookie/imad/domain/contents/dto/DetailsMovie.java
+++
@@ -1,6 +0,0 @@
-package com.ncookie.imad.domain.contents.dto;
-
-public class DetailsMovie extends DetailsResponse {
-
-    String title;
-}
--- src/main/java/com/ncookie/imad/domain/contents/dto/DetailsTv.java
+++
@@ -1,5 +0,0 @@
-package com.ncookie.imad.domain.contents.dto;
-
-public class DetailsTv extends DetailsResponse {
-    String name;
-}
