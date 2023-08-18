d4b0414350b22386e5bb422f4c2b583b5930703b
NCookies <swstar21c@gmail.com>
2023-08-18 12:53:24 +0900

    #26 - TMDB details 정보를 담는 DTO 이름 변경 및 contents id 정보 추가
    - DTO 클래스 이름을 `DetailsResponse` -> `TmdbDetails`로 수정함
    - 기존에 `id` 필드에 들어가던 값은 tmdb 내부에서 사용하는 id였는데, IMAD 서비스의 contents 테이블에 저장되는 id도 추가하였음

--- src/main/java/com/ncookie/imad/domain/tmdb/dto/TmdbDetails.java
+++ src/main/java/com/ncookie/imad/domain/tmdb/dto/TmdbDetails.java
@@ -1,5 +1,75 @@
 package com.ncookie.imad.domain.tmdb.dto;
 
+import com.fasterxml.jackson.annotation.*;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import com.ncookie.imad.domain.contents.entity.ContentsType;
+import com.ncookie.imad.domain.season.dto.DetailsSeason;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true, allowSetters = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TmdbDetails {
+    private long tmdbId;
+    private long contentsId;
+
+    private String overview;
+    private String tagline;
+    private String posterPath;
+    private String originalLanguage;
+    private String certification;
+
+    private Set<Integer> genres;
+    private Set<String> productionCountries;
+
+
+    // IMAD Data
+    private ContentsType contentsType;
+
+
+    // Movie Data
+    private String title;
+    private String originalTitle;
+
+    private String releaseDate;
+    private int runtime;
+
+    private String status;
+
+
+    // TV Data
+    private String name;
+    private String originalName;
+
+    private String firstAirDate;
+    private String lastAirDate;
+
+    private int numberOfEpisodes;
+    private int numberOfSeasons;
+
+    private List<DetailsSeason> seasons;
+
+
+    @JsonCreator
+    public TmdbDetails(
+            @JsonProperty("id") int id,
+            @JsonProperty("genres") Set<DetailsGenre> genreSet,
+            @JsonProperty("production_countries") Set<ProductionCountry> productionCountrySet) {
+        this.tmdbId = id;
+
+        this.genres = new HashSet<>();
+        genreSet.forEach(genre -> this.genres.add(genre.getId()));
+
+        this.productionCountries = new HashSet<>();
+        productionCountrySet.forEach(country -> this.productionCountries.add(country.getIso_3166_1()));
+    }
 }
