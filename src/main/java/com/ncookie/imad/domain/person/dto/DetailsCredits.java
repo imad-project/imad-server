bad679b2dfa977d2642a0228e2ab0f975440a8ef
NCookies <swstar21c@gmail.com>
2023-08-20 20:19:40 +0900

    #26 - TmdbFeignClient의 getDetails() 메소드에서 type definition 발생하는 에러 해결
    - DTO 클래스인 DetailsCredits에서 @Builder 어노테이션을 사용하고 있는데, @NoArgsConstructor, @AllArgsConstructor를 적용하지 않아 제대로 매핑이 되지 않고 있었다.

--- src/main/java/com/ncookie/imad/domain/person/dto/DetailsCredits.java
+++ src/main/java/com/ncookie/imad/domain/person/dto/DetailsCredits.java
@@ -1,13 +1,17 @@
 package com.ncookie.imad.domain.person.dto;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Builder
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class DetailsCredits {
     @JsonProperty("cast")
     private List<DetailsPerson> cast;
