2542a210348e781e3acab4636a0cb63c8af819ae
NCookies <swstar21c@gmail.com>
2023-08-20 15:36:23 +0900

    #26 - Person entity 수정
    - 칼럼 이름 카멜 표기법에 맞게 변경
    - Person - Credit 관계는 단방향이어도 문제가 없으므로 credit 참조 칼럼 삭제
    - toEntity() 메소드 추가

--- src/main/java/com/ncookie/imad/domain/person/entity/Person.java
+++ src/main/java/com/ncookie/imad/domain/person/entity/Person.java
@@ -1,11 +1,10 @@
 package com.ncookie.imad.domain.person.entity;
 
+import com.ncookie.imad.domain.person.dto.DetailsPerson;
 import com.ncookie.imad.domain.user.entity.Gender;
 import jakarta.persistence.*;
 import lombok.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @Getter
 @Builder
@@ -14,15 +13,20 @@ import java.util.List;
 @AllArgsConstructor
 public class Person {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long personId;
 
-    @OneToMany(mappedBy = "person")
-    private List<Credit> credit = new ArrayList<>();
-
-    private String original_name;
-    @Setter private String translated_name;
+    private String originalName;
+    @Setter private String translatedName;
 
     private Gender gender;
-    private String profile_path;
+    private String profilePath;
+
+    public static Person toEntity(DetailsPerson person) {
+        return Person.builder()
+                .personId(person.getId())
+                .originalName(person.getName())
+                .gender(person.getGender())
+                .profilePath(person.getProfilePath())
+                .build();
+    }
 }
