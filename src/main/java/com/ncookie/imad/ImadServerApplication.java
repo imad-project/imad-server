bd77295c8943a853d302ce592e5bdde6c00efeea
NCookies <swstar21c@gmail.com>
2023-09-05 21:30:26 +0900

    #40 - TODO 및 관련 주석 삭제

--- src/main/java/com/ncookie/imad/ImadServerApplication.java
+++ src/main/java/com/ncookie/imad/ImadServerApplication.java
@@ -2,7 +2,6 @@ package com.ncookie.imad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -10,8 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class ImadServerApplication {
 
 	public static void main(String[] args) {
-		// TODO: 모든 Entity의 필드를 래퍼 클래스 타입으로 변환해야 함 (int, long -> Integer, Long)
-		// null 값을 할당할 수 있게 해야 함
 		SpringApplication.run(ImadServerApplication.class, args);
 	}
 }
