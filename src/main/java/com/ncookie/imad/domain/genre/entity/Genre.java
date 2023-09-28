5f6db68368e82c4513ff59845b8791c41080cf37
NCookies <swstar21c@gmail.com>
2023-09-28 11:59:30 +0900

    #62 - unused import statement WARNING 해결

--- src/main/java/com/ncookie/imad/domain/genre/entity/Genre.java
+++ src/main/java/com/ncookie/imad/domain/genre/entity/Genre.java
@@ -1,8 +1,6 @@
 package com.ncookie.imad.domain.genre.entity;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.*;
 
