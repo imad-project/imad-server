b47de69b506743c79fa152fb769b2f3bb5bd8e2e
NCookies <swstar21c@gmail.com>
2023-09-28 11:59:54 +0900

    #62 - 사용하지 않는 클래스 파일 삭제 또는 이름 변경

--- src/main/java/com/ncookie/imad/domain/genre/repository/UserPreferredGenreRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ncookie.imad.domain.genre.repository;
-
-import com.ncookie.imad.domain.genre.entity.UserPreferredGenre;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserPreferredGenreRepository extends JpaRepository<UserPreferredGenre, Long> {
-}
