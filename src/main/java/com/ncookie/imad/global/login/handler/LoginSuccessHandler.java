9b9b4044b7046b324fbdb05b53331eb643514529
NCookies <swstar21c@gmail.com>
2023-10-09 22:41:07 +0900

    #70 - onAuthenticationSuccess() 메소드 수정
    - 기존의 불필요한 람다식 사용 때문에 중복되던 코드를 정리하였다.
    - 로그 메세지에 더 상세한 정보를 포함하도록 하였다.
      - 토큰의 만료 시간의 경우 단순히 밀리세컨드만 주는 것이 아닌, 실제 만료시점을 명시해주었다.

--- src/main/java/com/ncookie/imad/global/login/handler/LoginSuccessHandler.java
+++ src/main/java/com/ncookie/imad/global/login/handler/LoginSuccessHandler.java
@@ -2,6 +2,7 @@ package com.ncookie.imad.global.login.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ncookie.imad.domain.user.dto.response.UserInfoResponse;
+import com.ncookie.imad.domain.user.entity.UserAccount;
 import com.ncookie.imad.domain.user.repository.UserAccountRepository;
 import com.ncookie.imad.global.dto.response.ApiResponse;
 import com.ncookie.imad.global.dto.response.ResponseCode;
@@ -17,6 +18,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -28,6 +31,9 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
     @Value("${jwt.access.expiration}")
     private String accessTokenExpiration;
 
+    @Value("${jwt.refresh.expiration}")
+    private String refreshTokenExpiration;
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
                                         Authentication authentication) {
@@ -38,38 +44,37 @@ public class LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
         // 로그인 시 response에 유저 정보 첨부
         ObjectMapper mapper = new ObjectMapper();
-        userRepository.findByEmail(email).ifPresent(user -> {
-                    UserInfoResponse userInfoResponse = UserInfoResponse.builder()
-                            .email(user.getEmail())
-                            .nickname(user.getNickname())
-                            .authProvider(user.getAuthProvider())
-                            .gender(user.getGender())
-                            .ageRange(user.getAgeRange())
-                            .profileImage(user.getProfileImage())
-                            .role(user.getRole())
-                            .build();
+        Optional<UserAccount> optionalUserAccount = userRepository.findByEmail(email);
+        if (optionalUserAccount.isPresent()) {
+            UserAccount user = optionalUserAccount.get();
 
-                    try {
-                        response.setCharacterEncoding("UTF-8");
-                        response.getWriter().write(mapper.writeValueAsString(
-                                ApiResponse.createSuccess(ResponseCode.LOGIN_SUCCESS, userInfoResponse)));
-                        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
-                    } catch (IOException e) {
-                        throw new RuntimeException(e);
-                    }
-                }
-        );
+            UserInfoResponse userInfoResponse = UserInfoResponse.builder()
+                    .email(user.getEmail())
+                    .nickname(user.getNickname())
+                    .authProvider(user.getAuthProvider())
+                    .gender(user.getGender())
+                    .ageRange(user.getAgeRange())
+                    .profileImage(user.getProfileImage())
+                    .role(user.getRole())
+                    .build();
 
-        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
+            try {
+                response.setCharacterEncoding("UTF-8");
+                response.getWriter().write(mapper.writeValueAsString(
+                        ApiResponse.createSuccess(ResponseCode.LOGIN_SUCCESS, userInfoResponse)));
+                response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
 
-        userRepository.findByEmail(email)
-                .ifPresent(user -> {
-                    user.updateRefreshToken(refreshToken);
-                    userRepository.saveAndFlush(user);
-                });
-        log.info("로그인에 성공하였습니다. 이메일 : {}", email);
-        log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
-        log.info("발급된 AccessToken 만료 기간 : {}", accessTokenExpiration);
+            jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken); // 응답 헤더에 AccessToken, RefreshToken 실어서 응답
+            jwtService.updateRefreshToken(user.getEmail(), refreshToken);
+
+            log.info("로그인에 성공하였습니다. 이메일 : {}", email);
+            log.info("로그인에 성공하였습니다. AccessToken : {}", accessToken);
+            log.info("발급된 AccessToken 만료 기간 : {}", LocalDateTime.now().plusSeconds(Long.parseLong(accessTokenExpiration) / 1000));
+            log.info("발급된 RefreshToken 만료 기간 : {}", LocalDateTime.now().plusSeconds(Long.parseLong(refreshTokenExpiration) / 1000));
+        }
     }
 
     private String extractUsername(Authentication authentication) {
