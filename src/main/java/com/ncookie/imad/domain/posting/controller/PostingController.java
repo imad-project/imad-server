f6f4f12c26379e6c80e3ad2afd005f76dbdb374c
NCookies <swstar21c@gmail.com>
2023-10-14 02:16:13 +0900

    #72 - PostingController @Description 추가

--- src/main/java/com/ncookie/imad/domain/posting/controller/PostingController.java
+++ src/main/java/com/ncookie/imad/domain/posting/controller/PostingController.java
@@ -4,6 +4,7 @@ import com.ncookie.imad.domain.posting.dto.*;
 import com.ncookie.imad.domain.posting.service.PostingService;
 import com.ncookie.imad.global.dto.response.ApiResponse;
 import com.ncookie.imad.global.dto.response.ResponseCode;
+import jdk.jfr.Description;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,12 +14,14 @@ import org.springframework.web.bind.annotation.*;
 public class PostingController {
     private final PostingService postingService;
 
+    @Description("게시글 조회")
     @GetMapping("/{id}")
     public ApiResponse<PostingDetailsResponse> postingDetails(@PathVariable Long id) {
         return ApiResponse.createSuccess(ResponseCode.POSTING_GET_DETAILS_SUCCESS,
                 postingService.getPosting(id));
     }
 
+    @Description("게시글 등록")
     @PostMapping("")
     public ApiResponse<PostingIdResponse> postingAdd(@RequestHeader("Authorization") String accessToken,
                                                       @RequestBody AddPostingRequest addPostingRequest) {
@@ -27,6 +30,7 @@ public class PostingController {
                 postingService.addPosting(accessToken, addPostingRequest));
     }
 
+    @Description("게시글 수정")
     @PatchMapping("/{id}")
     public ApiResponse<PostingIdResponse> postingModify(@RequestHeader("Authorization") String accessToken,
                                                         @PathVariable("id") Long id,
@@ -35,6 +39,7 @@ public class PostingController {
                 postingService.modifyPosting(accessToken, id, modifyPostingRequest));
     }
 
+    @Description("게시글 삭제")
     @DeleteMapping("/{id}")
     public ApiResponse<?> postingDelete(@RequestHeader("Authorization") String accessToken,
                                          @PathVariable Long id) {
@@ -42,12 +47,14 @@ public class PostingController {
         return ApiResponse.createSuccessWithNoContent(ResponseCode.POSTING_DELETE_DETAILS_SUCCESS);
     }
 
+    @Description("게시글 리스트 전체 조회")
     @GetMapping("/list")
     public ApiResponse<PostingListResponse> postingList(@RequestHeader("Authorization") String accessToken,
                                                         @RequestParam(value = "page") int page) {
         return ApiResponse.createSuccess(ResponseCode.POSTING_GET_LIST_SUCCESS, postingService.getAllPostingList(accessToken, page));
     }
 
+    @Description("게시글 리스트 조건부 조회")
     @GetMapping("/list/search")
     public ApiResponse<PostingListResponse> postingListByQuery(@RequestHeader("Authorization") String accessToken,
                                                 @RequestParam(value = "search_type") int searchType,
