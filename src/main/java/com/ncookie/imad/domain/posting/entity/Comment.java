10ed8367440ba452bc1d72882756897846cc60ac
NCookies <swstar21c@gmail.com>
2023-10-15 20:09:09 +0900

    #72 - Comment 테이블에서 order 필드 삭제
    - 댓글 순서를 구분하기 위해서 넣은 필드이지만, 실제로는 날짜순으로 정렬하면 되기 때문에 의미가 없는 데이터이어서 제거했다.

--- src/main/java/com/ncookie/imad/domain/posting/entity/Comment.java
+++ src/main/java/com/ncookie/imad/domain/posting/entity/Comment.java
@@ -29,11 +29,8 @@ public class Comment extends BaseTimeEntity {
     private UserAccount userAccount;
     
     // 부모 댓글. 답글을 달았을 때 최상위 댓글 아래에 순차적으로 등록됨
-    private Long parentComment;
+    private Long parentId;
 
     @Setter
     private String content;
-
-    @Setter
-    private int commentOrder;
 }
