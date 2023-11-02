63a59ddcfde4d7307e15057f764d85775b9d1183
NCookies <swstar21c@gmail.com>
2023-11-02 21:23:23 +0900

    #99 - #101: 나머지 클래스들에도 적용

--- src/main/java/com/ncookie/imad/domain/review/dto/response/ReviewListResponse.java
+++ src/main/java/com/ncookie/imad/domain/review/dto/response/ReviewListResponse.java
@@ -2,42 +2,20 @@ package com.ncookie.imad.domain.review.dto.response;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
-import lombok.Builder;
-import lombok.Data;
+import com.ncookie.imad.domain.common.dto.ListResponse;
+import lombok.experimental.SuperBuilder;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
 
-@Data
-@Builder
+@SuperBuilder
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class ReviewListResponse {
-    List<ReviewDetailsResponse> reviewDetailsResponseList;      // 리뷰 정보
-
-    long totalElements;              // 총 리뷰 개수
-    long totalPages;                 // 총 페이지 수
-
-    int pageNumber;                 // 현재 페이지
-    int numberOfElements;           // 현재 페이지의 리뷰 개수
-    int sizeOfPage;                 // 한 페이지 당 최대 리뷰 개수
-
-    int sortDirection;              // 0 : 오름차순, 1 : 내림차순
-    String sortProperty;            // 정렬 기준 (score, createdDate, likeCnt, dislikeCnt 등이 있음)
-
+public class ReviewListResponse extends ListResponse<ReviewDetailsResponse> {
     public static ReviewListResponse toDTO(Page<?> page, List<ReviewDetailsResponse> reviewList) {
-        String sortProperty = null;
-        int sortDirection = 0;
-        Sort sort = page.getSort();
-        List<Sort.Order> orders = sort.toList();
-        for (Sort.Order order : orders) {
-            sortProperty = order.getProperty();
-            sortDirection = order.getDirection().name().equals("ASC") ? 0 : 1;
-        }
-
+        SortVariable sortVariable = getSortVariable(page);
         return ReviewListResponse.builder()
-                .reviewDetailsResponseList(reviewList)
+                .detailsList(reviewList)
 
                 .totalElements(page.getTotalElements())
                 .totalPages(page.getTotalPages())
@@ -46,8 +24,8 @@ public class ReviewListResponse {
                 .numberOfElements(page.getNumberOfElements())
                 .sizeOfPage(page.getSize())
 
-                .sortDirection(sortDirection)
-                .sortProperty(sortProperty)
+                .sortDirection(sortVariable.getSortDirection())
+                .sortProperty(sortVariable.getSortProperty())
 
                 .build();
     }
