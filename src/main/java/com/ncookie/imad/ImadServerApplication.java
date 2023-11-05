9fbca390e89078123c88f7c43f960d638eeeab0f
NCookies <swstar21c@gmail.com>
2023-11-05 20:09:58 +0900

    #110 - Redis 연동 구현 및 연결 테스트 성공
    - Windows 10 환경에 Redis 설치 후 해당 설정을 application.yml 파일에 적용했다.
    - Redis DB를 사용하기 위한 configuration 파일을 생성했다.
    - 간단한 테스트를 통해 연동이 성공적으로 이루어졌음을 확인했다.

--- src/main/java/com/ncookie/imad/ImadServerApplication.java
+++ src/main/java/com/ncookie/imad/ImadServerApplication.java
@@ -2,12 +2,13 @@ package com.ncookie.imad;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class ImadServerApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ImadServerApplication.class, args);
 	}
