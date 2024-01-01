d543c0fb519b5ebd432138dca2fb14aa9f677217
NCookies <swstar21c@gmail.com>
2024-01-01 18:24:57 +0900

    #110 - #149: 총합 작품 랭킹 점수 저장할 entity 및 repository 생성

---
+++ src/main/java/com/ncookie/imad/domain/ranking/entity/ContentsEntireScore.java
@@ -0,0 +1,28 @@
+package com.ncookie.imad.domain.ranking.entity;
+
+import com.ncookie.imad.domain.contents.entity.Contents;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Builder
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class ContentsEntireScore {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "contents_id")
+    @ToString.Exclude
+    private Contents contents;
+
+    @Setter
+    private int rankingScore;
+}
---
+++ src/main/java/com/ncookie/imad/domain/ranking/repository/ContentsEntireScoreRepository.java
@@ -0,0 +1,7 @@
+package com.ncookie.imad.domain.ranking.repository;
+
+import com.ncookie.imad.domain.ranking.entity.ContentsEntireScore;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ContentsEntireScoreRepository extends JpaRepository<ContentsEntireScore, Long> {
+}
