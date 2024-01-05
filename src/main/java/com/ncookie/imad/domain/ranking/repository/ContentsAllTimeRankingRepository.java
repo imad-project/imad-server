8f029249f484146ce2da72db52081e3153c8fb0a
NCookies <swstar21c@gmail.com>
2024-01-06 00:00:35 +0900

    #110 - #150: 더 이상 사용되지 않는 클래스 삭제
    - MySQL에서 사용될 것으로 설계된 데이터들은 대부분 Redis를 사용하게 되었다.
    - 때문에 기존에 만들어두었던 entity 및 repository는 더 이상 쓸모가 없어 삭제했다.

--- src/main/java/com/ncookie/imad/domain/ranking/repository/ContentsAllTimeRankingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ncookie.imad.domain.ranking.repository;
-
-import com.ncookie.imad.domain.ranking.entity.ContentsAllTimeRanking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContentsAllTimeRankingRepository extends JpaRepository<ContentsAllTimeRanking, Long> {
-}
--- src/main/java/com/ncookie/imad/domain/ranking/repository/ContentsWeeklyRankingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ncookie.imad.domain.ranking.repository;
-
-import com.ncookie.imad.domain.ranking.entity.ContentsWeeklyRanking;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContentsWeeklyRankingRepository extends JpaRepository<ContentsWeeklyRanking, Long> {
-}
