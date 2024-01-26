5d24627594a5d1c767c67f3276c180a23a97055c
NCookies <swstar21c@gmail.com>
2024-01-26 20:43:02 +0900

    #157 - #162: 작품 타입별 랭킹 조회 repository 메소드 구현

--- src/main/java/com/ncookie/imad/domain/ranking/repository/RankingMonthlyRepository.java
+++ src/main/java/com/ncookie/imad/domain/ranking/repository/RankingMonthlyRepository.java
@@ -1,7 +1,12 @@
 package com.ncookie.imad.domain.ranking.repository;
 
+import com.ncookie.imad.domain.contents.entity.ContentsType;
 import com.ncookie.imad.domain.ranking.entity.RankingMonthly;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RankingMonthlyRepository extends JpaRepository<RankingMonthly, Long> {
+    Page<RankingMonthly> findAll(Pageable pageable, ContentsType contentsType);
+    Page<RankingMonthly> findAllByContentsType(Pageable pageable, ContentsType contentsType);
 }
--- src/main/java/com/ncookie/imad/domain/ranking/repository/RankingWeeklyRepository.java
+++ src/main/java/com/ncookie/imad/domain/ranking/repository/RankingWeeklyRepository.java
@@ -1,7 +1,13 @@
 package com.ncookie.imad.domain.ranking.repository;
 
+import com.ncookie.imad.domain.contents.entity.ContentsType;
 import com.ncookie.imad.domain.ranking.entity.RankingWeekly;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RankingWeeklyRepository extends JpaRepository<RankingWeekly, Long> {
+    Page<RankingWeekly> findAll(Pageable pageable, ContentsType contentsType);
+
+    Page<RankingWeekly> findAllByContentsType(Pageable pageable, ContentsType contentsType);
 }
