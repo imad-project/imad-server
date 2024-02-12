a2590c117bc642b4c0828d5d52ba0c3c2e07ec67
NCookies <swstar21c@gmail.com>
2024-02-12 23:23:14 +0900

    #170 - 소셜 로그인 성공 시 JWT access token 정보를 담은 상태로 리디렉션 되도록 함

--- src/main/java/com/ncookie/imad/global/oauth2/handler/OAuth2LoginSuccessHandler.java
+++ src/main/java/com/ncookie/imad/global/oauth2/handler/OAuth2LoginSuccessHandler.java
@@ -1,6 +1,5 @@
 package com.ncookie.imad.global.oauth2.handler;
 
-import com.ncookie.imad.global.Utils;
 import com.ncookie.imad.global.jwt.property.JwtProperties;
 import com.ncookie.imad.global.jwt.service.JwtService;
 import com.ncookie.imad.global.oauth2.CustomOAuth2User;
@@ -9,11 +8,15 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.web.DefaultRedirectStrategy;
+import org.springframework.security.web.RedirectStrategy;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 
 @Slf4j
@@ -25,10 +28,24 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
     private final JwtService jwtService;
     private final JwtProperties jwtProperties;
 
+    private final RedirectStrategy redirectStrategy = new DefaultRedirectStrategy();
+
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
         log.info("OAuth2 Login 성공!");
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+
+        String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
+        String refreshToken = jwtService.createRefreshToken();
+
+        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+        jwtService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
+
+        String redirectUrl = "https://iimad.com/oauth2/redirect/" + accessToken;
+
+        redirectStrategy.sendRedirect(request, response, redirectUrl);
+
         loginSuccess(response, oAuth2User);     // 로그인에 성공한 경우 access, refresh 토큰 생성
     }
 
@@ -41,7 +58,7 @@ public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
         jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
         jwtService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
 
-        Utils.sendLoginSuccessResponse(response);
+//        Utils.sendLoginSuccessResponse(response);
 
         log.info("로그인에 성공하였습니다.");
         log.info("이메일 : {}", oAuth2User.getEmail());
