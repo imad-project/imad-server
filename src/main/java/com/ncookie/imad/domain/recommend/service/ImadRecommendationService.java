18aba00b8b648d5f2469252f22e10e2f54a2b388
NCookies <swstar21c@gmail.com>
2024-04-21 23:41:19 +0900

    #179 - 주석 추가

--- src/main/java/com/ncookie/imad/domain/recommend/service/ImadRecommendationService.java
+++ src/main/java/com/ncookie/imad/domain/recommend/service/ImadRecommendationService.java
@@ -5,4 +5,5 @@ import org.springframework.context.annotation.Description;
 @Description("IMAD 자체 작품 추천 관련 서비스")
 public class ImadRecommendationService {
     // IMAD에서 인기 있는 작품 추천
+    // TMDB API Top Rated
 }
--- src/main/java/com/ncookie/imad/domain/recommend/service/TrendBasedRecommendationService.java
+++ src/main/java/com/ncookie/imad/domain/recommend/service/TrendBasedRecommendationService.java
@@ -5,4 +5,5 @@ import org.springframework.context.annotation.Description;
 @Description("현재 인기 작품 추천 관련 서비스")
 public class TrendBasedRecommendationService {
     // TMDB API 참고
+    // Trend, Popular 등
 }
