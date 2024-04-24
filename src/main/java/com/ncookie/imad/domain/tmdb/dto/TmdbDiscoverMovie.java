709287fbd8e0082980481b8c524b5d626ac57944
NCookies <swstar21c@gmail.com>
2024-04-25 00:17:52 +0900

    #179 - 선호 장르 기반 관련 DTO 클래스 구현
    - TMDB API의 `discover`에 요청할 때 데이터를 받아올 DTO 클래스 구현 및 테스트 완료

--- src/main/java/com/ncookie/imad/domain/tmdb/dto/TmdbDiscoverMovie.java
+++ src/main/java/com/ncookie/imad/domain/tmdb/dto/TmdbDiscoverMovie.java
@@ -1,13 +1,21 @@
 package com.ncookie.imad.domain.tmdb.dto;
 
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
+import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true, allowSetters = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
 public class TmdbDiscoverMovie {
+    // 페이지 정보
+    private Long page;
+    private Long totalPages;
+    private Long totalResults;
+
+    private List<TmdbDiscoverMovieDetails> results;
 }
--- src/main/java/com/ncookie/imad/domain/tmdb/dto/TmdbDiscoverMovieDetails.java
+++ src/main/java/com/ncookie/imad/domain/tmdb/dto/TmdbDiscoverMovieDetails.java
@@ -1,21 +1,16 @@
 package com.ncookie.imad.domain.tmdb.dto;
 
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.Data;
 
-import java.util.List;
 
 @Data
 @JsonIgnoreProperties(ignoreUnknown = true, allowSetters = true)
 @JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
-public class TmdbDiscoverTv {
-        // 페이지 정보
-    private Long page;
-    private Long totalPages;
-    private Long totalResults;
-
-    private List<TmdbDiscoverTvDetails> results;
+public class TmdbDiscoverMovieDetails {
+    private Long id;                // TMDB ID
+    private String title;           // 제목
+    private String posterPath;      // 포스터 경로
 }
