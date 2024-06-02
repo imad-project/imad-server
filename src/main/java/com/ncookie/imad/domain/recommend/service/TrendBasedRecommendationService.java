c9f4b9256ba0847bece91d042aa344218123ba47
NCookies <swstar21c@gmail.com>
2024-06-03 01:31:57 +0900

    #179 - 불필요한 Service 클래스 제거
    - 설계 초기에는 각각의 추천 기능을 서비스로 만드려고 했으나 함수 하나씩만 가지고 있기 때문에 하나의 서비스로 합쳤다.
    - 활동 기반 추천 기능에 대해서는 실제로 구현해보면서 판단해야 될 것 같다.

--- src/main/java/com/ncookie/imad/domain/recommend/service/TrendBasedRecommendationService.java
+++
@@ -1,9 +0,0 @@
-package com.ncookie.imad.domain.recommend.service;
-
-import org.springframework.context.annotation.Description;
-
-@Description("현재 인기 작품 추천 관련 서비스")
-public class TrendBasedRecommendationService {
-    // TMDB API 참고
-    // Trend, Popular 등
-}
