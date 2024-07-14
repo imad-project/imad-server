bb3eb59393625be51510dca29c2c8a1800965f7d
NCookies <swstar21c@gmail.com>
2024-07-14 13:08:16 +0900

    #215 - 파일 용량이 너무 클 때의 예외처리

--- src/main/java/com/ncookie/imad/global/advice/GlobalAdviceController.java
+++ src/main/java/com/ncookie/imad/global/advice/GlobalAdviceController.java
@@ -1,15 +1,24 @@
 package com.ncookie.imad.global.advice;
 
 import com.ncookie.imad.global.dto.response.ApiResponse;
+import com.ncookie.imad.global.dto.response.ResponseCode;
 import com.ncookie.imad.global.exception.BadRequestException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.multipart.MultipartException;
+
+import javax.naming.SizeLimitExceededException;
 
 @RestControllerAdvice
 public class GlobalAdviceController {
     @ExceptionHandler(value = BadRequestException.class)
     protected ResponseEntity<ApiResponse<?>> badRequestException(BadRequestException e) {
         return ResponseEntity.badRequest().body(ApiResponse.createError(e.getResponseCode()));
+    }
+
+    @ExceptionHandler(value = MultipartException.class)
+    protected ResponseEntity<ApiResponse<?>> fileSizeLimitExceededException() {
+        return ResponseEntity.badRequest().body(ApiResponse.createError(ResponseCode.FILE_MAX_UPLOAD_SIZE_EXCEEDED));
     }
 }
