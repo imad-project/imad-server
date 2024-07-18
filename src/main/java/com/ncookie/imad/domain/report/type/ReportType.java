1f99961fa6331cb02d3084881daa5e8aef509fd9
NCookies <swstar21c@gmail.com>
2024-07-18 22:48:56 +0900

    #220 - 신고 사유 추가

--- src/main/java/com/ncookie/imad/domain/report/type/ReportType.java
+++ src/main/java/com/ncookie/imad/domain/report/type/ReportType.java
@@ -1,7 +1,8 @@
 package com.ncookie.imad.domain.report.type;
 
 public enum ReportType {
-    SPAM("SPAM"),                                   // 스팸
+    WRONG_INFO("WRONG_INFO"),                       // 잘못된 정보
+    SPAM("SPAM"),                                   // 스팸, 상업적 광고
     ABUSIVE("ABUSIVE"),                             // 폭력적이거나 공격적인 내용
     INAPPROPRIATE("INAPPROPRIATE"),                 // 부적절한 내용 (성적인 컨텐츠, 혐오 발언 등)
     COPYRIGHT_VIOLATION("COPYRIGHT_VIOLATION"),     // 저작권 침해
